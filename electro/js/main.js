9b367e11d86e09c4cc40e88b9363dc14569015ec
amayro <aabmstu@gmail.com>
2019-02-10 22:27:45 +0300

    hw_02

--- electro/js/main.js
+++ electro/js/main.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const products = [
+const productsData = [
     {
         title: 'Notebook',
         category: 'Laptops',
@@ -40,36 +40,104 @@ const products = [
 
 ];
 
-
-const renderProduct = (title, category, image, price, sale, kind, rating) => {
-    let oldPrice = null;
-    if (sale) {
-        oldPrice = price;
-        price = oldPrice * (1 - sale/100);
+class ProductsList {
+    constructor(container = '.products-slick') {
+        this.container = container;
+        this.goods = [];
+        this.allProducts = [];
+        this._getProducts();
     }
 
-    const bl_sale = `<span class="sale">-${sale}%</span>`;
-    const bl_kind = `<span class="new">${kind}</span>`;
+    _getProducts() {
+        this.goods = productsData;
+    }
 
-    const block = `<div class="product">
+    getProductsPrice() {
+        let price = 0;
+        this.allProducts.forEach(el => price += el.price);
+        return price
+    }
+
+    render() {
+        const block = document.querySelector(this.container);
+        for (let product of this.goods) {
+            const productObj = new ProductItem(
+                product.title,
+                product.category,
+                product.image,
+                product.price,
+                product.sale,
+                product.kind,
+                product.rating
+            );
+            this.allProducts.push(productObj);
+            block.insertAdjacentHTML('beforeend', productObj.render());
+        }
+        block.insertAdjacentHTML('beforebegin', `<h5>Total cost all products: ${this.getProductsPrice()}</h5>`);
+    }
+}
+
+class ProductItem {
+    constructor(title, category, image, price, sale, kind, rating) {
+        this.title = title;
+        this.category = category;
+        this.image = image;
+        this.price = price;
+        this.sale = sale;
+        this.kind = kind;
+        this.rating = rating;
+    }
+
+    blockSale() {
+        const blSale = `<span class="sale">-${this.sale}%</span>`;
+        return this.sale ? blSale : ''
+    }
+
+    blockKind() {
+        const blKind = `<span class="new">${this.kind}</span>`;
+        return this.kind ? blKind : ''
+    }
+
+    blockPrice() {
+        let oldPrice = null;
+        let blOldPrice = '';
+
+        if (this.sale) {
+            oldPrice = this.price;
+            this.price = oldPrice * (1 - this.sale / 100);
+            blOldPrice = `<del class="product-old-price">$${oldPrice}</del>`;
+        }
+
+        return `<h4 class="product-price">$${this.price} ${blOldPrice}</h4>`
+    }
+
+    blockRating() {
+        let rating_stars = '';
+
+        for (let i = 0; i < this.rating; i++) {
+            rating_stars += '<i class="fa fa-star"></i>'
+        }
+        for (let i = 0; i < 5 - this.rating; i++) {
+            rating_stars += '<i class="fa fa-star-o"></i>'
+        }
+        return `<div class="product-rating">${rating_stars}</div>`
+    }
+
+    render() {
+        return `<div class="product">
                         <div class="product-img">
-                            <img src="${image}" alt="">
+                            <img src="${this.image}" alt="">
                             <div class="product-label">
-                                ${bl_sale}
-                                ${bl_kind}
+                                ${this.blockSale()}
+                                ${this.blockKind()}
                             </div>
                         </div>
                         <div class="product-body">
-                            <p class="product-category">${category}</p>
-                            <h3 class="product-name"><a href="#">${title}</a></h3>
-                            <h4 class="product-price">$${price} <del class="product-old-price">$${oldPrice}</del></h4>
-                            <div class="product-rating">
-                                <i class="fa fa-star"></i>
-                                <i class="fa fa-star"></i>
-                                <i class="fa fa-star"></i>
-                                <i class="fa fa-star"></i>
-                                <i class="fa fa-star"></i>
-                            </div>
+                            <p class="product-category">${this.category}</p>
+                            <h3 class="product-name"><a href="#">${this.title}</a></h3>
+                            
+                            ${this.blockPrice()}
+                            ${this.blockRating()}
                             <div class="product-btns">
                                 <button class="add-to-wishlist"><i class="fa fa-heart-o"></i><span class="tooltipp">add to wishlist</span></button>
                                 <button class="add-to-compare"><i class="fa fa-exchange"></i><span class="tooltipp">add to compare</span></button>
@@ -79,14 +147,40 @@ const renderProduct = (title, category, image, price, sale, kind, rating) => {
                         <div class="add-to-cart">
                             <button class="add-to-cart-btn"><i class="fa fa-shopping-cart"></i> add to cart</button>
                         </div>
-                    </div>`;
-    return block
-};
+                    </div>`
+    }
+}
 
-const renderPage = list => {
-    const productList = list.map(item => renderProduct(...Object.values(item)));
-    document.querySelector('.products-slick').innerHTML = productList.join('');
-};
+class Basket {
+    constructor(containerSelector = '#cart',
+                countSelector = '.basket-count',
+                priceSelector = '#basket-price') {
+        this.containerSelector = containerSelector;
+        this.countSelector = countSelector;
+        this.priceSelector = priceSelector;
+        this.orders = [];
+    }
 
-renderPage(products);
+    getOrdersCount() {
+        let count = 0;
+        this.orders.forEach((el) => count += el.count);
+        return count
+    }
 
+
+    getOrdersPrice() {
+        let price = 0;
+        this.orders.forEach((el) => price += el.price * el.count);
+        return price
+    }
+
+    render() {
+        document.querySelector(this.countSelector).innerText = this.getOrdersCount();
+        document.querySelector(this.priceSelector).innerText = this.getOrdersPrice();
+    }
+
+}
+
+
+let products = new ProductsList();
+products.render();
